830133af5937eabebb24040720abcb00d680dcaf
apatel28 <apatel28@ncsu.edu>
2021-09-01 19:55:26 -0400

    Update calculator.py

--- code/calculator.py
+++ code/calculator.py
@@ -1,20 +1,30 @@
 class calculator:
 	@staticmethod
 	def add(num1, num2):
-		return (num1 + num2)
+		if (type(num1) == int or float) and (type(num2) == int or float):
+			return (num1 + num2)
+		else:
+			return 'Invalid: Please enter a valid number'
 
 	@staticmethod
 	def subtract(num1, num2):
-		return (num1 - num2)
+		if (type(num1) == int or float) and (type(num2) == int or float):
+			return (num1 - num2)
+		else:
+			return 'Invalid: Please enter a valid number'
 	
 	@staticmethod
 	def multiply(num1, num2):
-		return (num1 * num2)
+		if (type(num1) == int or float) and (type(num2) == int or float):
+			return (num1 * num2)
+		else:
+			return 'Invalid: Please enter a valid number'
 
 	@staticmethod
 	def divide(num1, num2):
-		if num2==0:
-			return "You can't divide by zero!"
-		return (num1 / num2)
-	
-	
+		if (type(num1) == int or float) and (type(num2) == int or float):
+			if num2==0:
+				return "You can't divide by zero!"
+			return (num1 / num2)
+		else:
+			return 'Invalid: Please enter a valid number'	
