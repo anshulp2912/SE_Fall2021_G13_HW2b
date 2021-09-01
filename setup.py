19d45e9f5b8e5b9c71849aa32a7f1c92823a3d87
AgrawalBhavya <52067226+AgrawalBhavya@users.noreply.github.com>
2021-09-01 10:49:28 -0400

    Added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SE_Fall2021_G13_HW2b",
+    maintainer="Bhavya Omprakash Agrawal, Pragna Bollam, Darshan Patel, Anshul Patel, Rohan Shah",
+    version="0.0.1",
+    author="Anshul Patel",
+    author_email="agrawal.bhavya8@gmail.com",
+    description="Making a Good Repo",
+    url="https://github.com/anshulp2912/SE_Fall2021_G13_HW2b",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+    ),
+)
