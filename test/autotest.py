17e534cdcdca2ed8c5437225d84f72874a8e0a26
rohan22shah <42119767+rohan22shah@users.noreply.github.com>
2021-09-02 03:04:48 +0530

    Create autotest.py

---
+++ test/autotest.py
@@ -0,0 +1,14 @@
+import code.calculator.calculator
+def test_add():
+    assert add(2, 3) == 5 , 'TEST_PASS'
+    assert add('xyz','abc') != 'xyzabc' , "TEST_FAIL:Please enter numeric values only"
+    
+def test_subtract():
+  assert subtract(10,4) == 6 , "TEST_PASS"
+  
+def test_multiply():
+  assert multiply(10,6) == 60 , "TEST_PASS"
+  assert multiply("abc",2) != "abcabc" , "TEST_FAIL:Please enter numeric values only"
+
+def test_divide():
+  assert divide(10,5) == 2 , "TEST_PASS"
